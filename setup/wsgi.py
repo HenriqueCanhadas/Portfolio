59f7fa6bc96dfd9f9b0c2b8a74e563fb63cbfc37
Henrique Canhadas <pedrosacanhadas@gmail.com>
2025-05-09 18:39:18 -0300

    att vercel

--- setup/wsgi.py
+++ setup/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
 
 application = get_wsgi_application()
+
+#VERCEL
+# api/wsgi.py
+app = get_wsgi_application()
