43aafd9d65a62a22964784d6c97f90d36bf8685a
Henrique Canhadas <pedrosacanhadas@gmail.com>
2025-05-13 17:11:53 -0300

    Corrige ALLOWED_HOSTS para produção

--- setup/settings.py
+++ setup/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
