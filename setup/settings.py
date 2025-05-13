5a271f739433fa621b2ed128379f726dedc63c7e
Henrique Canhadas <pedrosacanhadas@gmail.com>
2025-05-13 17:35:52 -0300

    att

--- setup/settings.py
+++ setup/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'portfolio-dnkt.onrender']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
