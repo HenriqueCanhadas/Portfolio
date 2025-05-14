dfeacfdfd65c73663e9b76d76d48e157cdd02b02
Henrique Canhadas <pedrosacanhadas@gmail.com>
2025-05-14 17:12:28 -0300

    Att Funcionando com Banco de Dados

---
+++ portfolio/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from portfolio.views import detalhe_certificado, index, certificado
+
+urlpatterns = [
+        path('', index, name='index'),
+        path('certificado/', certificado, name='certificado'),
+        path('certificado/<str:linguagem>/', detalhe_certificado, name='detalhe_certificado')
+]
