f92ba5a1d5a63ac223f52038f6a1376e1df021c8
Henrique Canhadas <pedrosacanhadas@gmail.com>
2025-05-14 16:28:24 -0300

    att 0

--- portfolio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from portfolio.views import detalhe_certificado, index, certificado
-
-urlpatterns = [
-        path('', index, name='index'),
-        path('certificado/', certificado, name='certificado'),
-        path('certificado/<str:linguagem>/', detalhe_certificado, name='detalhe_certificado')
-]
-
