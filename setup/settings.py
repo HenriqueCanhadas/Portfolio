516cc9eac023c07009d08ba6503360537cace690
Henrique Canhadas <pedrosacanhadas@gmail.com>
2025-05-14 17:29:51 -0300

    att

--- setup/settings.py
+++ setup/settings.py
@@ -15,7 +15,6 @@ SECRET_KEY = 'django-insecure-*t@#f6dhhj7m1)b^4h#g%)4hl+e=^4ulx$%ia1aq57^(f0x1$%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 INSTALLED_APPS = [
